3218a98dfae7eb8b8296c716d5177be3d65fb61a
Ayaya62 <andhikayogapratama@mail.ugm.ac.id>
2023-10-31 22:48:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasbottomnav/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.example.tugasbottomnav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.tugasbottomnav.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() =_binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+            val action = HomeFragmentDirections.actionHomeFragment2ToListFragment()
+            btnExplore.setOnClickListener(){
+                findNavController().navigate(action)
+            }
+        }
+    }
+}
